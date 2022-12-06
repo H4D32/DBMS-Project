676f92c66e9894d22f8b46382b59ccf0434a448e
UGameGerm <christoph.chen@foxmail.com>
2022-12-07 02:07:54 +0800

    Fix: correct a typo in TableTest

--- DB61B/db61b/src/test/java/t3/db61b/TableTest.java
+++ DB61B/db61b/src/test/java/t3/db61b/TableTest.java
@@ -3,36 +3,35 @@ package t3.db61b;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
-public class TableTest 
-{
+public class TableTest {
     /**
      * Rigorous Test :-)
      */
-    public Table table = new Table(new String[] {"Student", "Mentor", "School"});
-    
+    public Table table = new Table(new String[] { "Student", "Mentor", "School" });
+
     @Test
     public void Test_writeTable() {
-        table.add(new Row(new String[] {"Alex", "Bob", "SDS"}));
-        table.add(new Row(new String[] {"Charlie", "Diana", "SSE"}));
+        table.add(new Row(new String[] { "Alex", "Bob", "SDS" }));
+        table.add(new Row(new String[] { "Charlie", "Diana", "SSE" }));
         table.writeTable(new String("student"));
     }
 
     @Test
     public void Test_print() {
-        table.add(new Row(new String[] {"Alex", "Bob", "SDS"}));
-        table.add(new Row(new String[] {"Charlie", "Diana", "SSE"}));
+        table.add(new Row(new String[] { "Alex", "Bob", "SDS" }));
+        table.add(new Row(new String[] { "Charlie", "Diana", "SSE" }));
         table.print();
     }
 
     @Test
     public void Test_readTable() {
-        table.add(new Row(new String[] {"Alex", "Bob", "SDS"}));
-        table.add(new Row(new String[] {"Charlie", "Diana", "SSE"}));
+        table.add(new Row(new String[] { "Alex", "Bob", "SDS" }));
+        table.add(new Row(new String[] { "Charlie", "Diana", "SSE" }));
         table.writeTable(new String("Student"));
         Table new_table = Table.readTable(new String("Student"));
         new_table.print();
@@ -40,10 +39,10 @@ public class TableTest
 
     @Test
     public void Test_columns() {
-        table.add(new Row(new String[] {"Alex", "Bob", "SDS"}));
-        table.add(new Row(new String[] {"Charlie", "Diana", "SSE"}));
+        table.add(new Row(new String[] { "Alex", "Bob", "SDS" }));
+        table.add(new Row(new String[] { "Charlie", "Diana", "SSE" }));
         assertEquals(3, table.columns());
-        Table new_table = new Table(new String[] {"A_Test", "B_Test"});
+        Table new_table = new Table(new String[] { "A_Test", "B_Test" });
         assertEquals(2, new_table.columns());
     }
 
@@ -55,22 +54,22 @@ public class TableTest
 
     @Test
     public void Test_findColumn() {
-        assertEquals("Mentor", table.findColumn(1));
-        assertEquals("School", table.findColumn(2));
+        assertEquals(1, table.findColumn("Mentor"));
+        assertEquals(2, table.findColumn("School"));
     }
 
     @Test
     public void Test_size() {
-        table.add(new Row(new String[] {"Alex", "Bob", "SDS"}));
+        table.add(new Row(new String[] { "Alex", "Bob", "SDS" }));
         assertEquals(1, table.size());
-        table.add(new Row(new String[] {"Charlie", "Diana", "SSE"}));
+        table.add(new Row(new String[] { "Charlie", "Diana", "SSE" }));
         assertEquals(2, table.size());
     }
 
     @Test
     public void Test_add() {
-        table.add(new Row(new String[] {"Test", "Add", "Success"}));
-        table.add(new Row(new String[] {"Print", "To", "Check"}));
+        table.add(new Row(new String[] { "Test", "Add", "Success" }));
+        table.add(new Row(new String[] { "Print", "To", "Check" }));
         table.print();
     }
 }
